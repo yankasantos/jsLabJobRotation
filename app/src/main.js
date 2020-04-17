766de94d0327df1ce83f24a44618ad0a70e53faf
yankasantos <yankasantos0107@gmail.com>
2020-04-17 17:19:48 -0300

    estudo de algumas funcionalidades em JavaScript, manipulando array, inserindo, deletando, revertendo a ordem e transformando em string

---
+++ app/src/main.js
@@ -0,0 +1,20 @@
+var nome = "Yanka Santos";
+var idade = 23;
+var nomeConvidade = "Renato Lelis";
+var idadeConvidade = 23;
+// alert(idade + idadeConvidade);
+// console.log(nome + idade + nomeConvidade + idadeConvidade);
+
+var listaEspera = ["Roberto", "Rayanne", "Landson"];
+listaEspera.push("Adelly", "Yesus");
+console.log(listaEspera);
+listaEspera.pop("Yesus");
+
+console.log(listaEspera);
+console.log(listaEspera[1]);
+console.log(listaEspera.length);
+console.log(listaEspera.reverse());
+console.log(listaEspera.toString());
+
+
+
